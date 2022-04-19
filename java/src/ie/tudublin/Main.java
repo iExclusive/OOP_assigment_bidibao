e3195c43f5aec280bbbe4b9e72dbc6a4ee80b242
C20338661 Ivan Medved <c20338661@mytudublin.ie>
2022-04-19 12:13:25 +0100

    update

---
+++ java/src/ie/tudublin/Main.java
@@ -0,0 +1,28 @@
+package ie.tudublin;
+
+
+
+public class Main
+{
+    public static void audio1()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new Audio1());
+    }
+
+    public static void audio2()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new Audio2());
+    }
+
+    
+    public static void main(String[] args)
+    {
+        audio1();
+
+        // Tara Misty
+        // Tara Tara
+        
+    }
+}
